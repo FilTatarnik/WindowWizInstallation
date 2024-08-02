d7e0b45ea6f6e879b5e27f193f0d3796f7fef6a2
Filip Tatarnik <ftatarnik@gmail.com>
2024-08-02 13:17:19 -0500

    added mobile responsiveness things like burger menu, additional css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
+import { Link } from 'react-scroll';
+
 class Navbar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            menuOpen: false
+        };
     }
-    state = {  }
-    
-    render() { 
-        return ( 
-            <nav className='navbar'>
-                    <Link to="home" smooth={true} duration={500} offset={0}>
-                            <h2>Window Wiz</h2>
-                    </Link>
-                <ul>
-                    <Link to="home" smooth={true} duration={500} offset={0}>
-                            <li>Home</li>
-                    </Link>
 
-                    <Link to="about" smooth={true} duration={500} offset={-125}>
+    toggleMenu = () => {
+        this.setState(prevState => ({
+            menuOpen: !prevState.menuOpen
+        }));
+    }
+
+    closeMenu = () => {
+        this.setState({ menuOpen: false });
+    }
+
+    render() {
+        return (
+            <>
+                {this.state.menuOpen && <div className="overlay" onClick={this.closeMenu}></div>}
+                <nav className='navbar'>
+                    <Link to="home" smooth={true} duration={500} offset={0}>
+                        <h1>Window Wiz</h1>
+                    </Link>
+                    <div className={`navbar-toggler ${this.state.menuOpen ? 'active' : ''}`} onClick={this.toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <ul className={this.state.menuOpen ? 'open' : ''}>
+                        <Link to="home" smooth={true} duration={500} offset={0} onClick={this.closeMenu}>
+                            <li>Home</li>
+                        </Link>
+                        <Link to="about" smooth={true} duration={500} offset={-125} onClick={this.closeMenu}>
                             <li>About</li>
-                    </Link>
-                    <Link to="services" smooth={true} duration={500} offset={0}>
+                        </Link>
+                        <Link to="services" smooth={true} duration={500} offset={0} onClick={this.closeMenu}>
                             <li>Services</li>
-                    </Link>
-                    <Link to="contact" smooth={true} duration={500} offset={0}>
+                        </Link>
+                        <Link to="contact" smooth={true} duration={500} offset={0} onClick={this.closeMenu}>
                             <li>Contact</li>
-                    </Link>
-                </ul>
-            </nav>
-         );
+                        </Link>
+                    </ul>
+                </nav>
+            </>
+        );
     }
 }
- 
+
 export default Navbar;
